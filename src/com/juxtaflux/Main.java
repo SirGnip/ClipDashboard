c663f660684a9f18576005dc1456d1183e595f92
SirGnip <SirGnip@users.noreply.github.com>
2017-10-19 17:30:52 -0500

    Add app icon, change path to fxml, add basic error handling for loading

--- src/com/juxtaflux/Main.java
+++ src/com/juxtaflux/Main.java
@@ -4,13 +4,32 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import java.io.InputStream;
 
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("../../ClipDashboard.fxml"));
-        Parent root = loader.load();
+        final String UI_LAYOUT_FILENAME = "/ClipDashboard.fxml";
+        final String ICON_FILENAME = "/ClipDashboardIcon.ico";
+        System.out.println("Application starting. Loading UI layout from " + UI_LAYOUT_FILENAME);
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(UI_LAYOUT_FILENAME));
+        Parent root;
+        try {
+            root = loader.load();
+        } catch (Exception e) {
+            System.out.println("ERROR: Problem loading UI layout: " + UI_LAYOUT_FILENAME);
+            throw e;
+        }
+
+        System.out.println("Loading app icon: " + ICON_FILENAME);
+        InputStream iconInputStream = getClass().getResourceAsStream(ICON_FILENAME);
+        if (iconInputStream == null) {
+            throw new Exception("Problem loading app icon: " + ICON_FILENAME);
+        }
+        primaryStage.getIcons().add(new Image(iconInputStream));
         primaryStage.setTitle(Config.APP_TITLE);
         primaryStage.setScene(new Scene(root, Config.APP_WIDTH, Config.APP_HEIGHT));
         primaryStage.show();
